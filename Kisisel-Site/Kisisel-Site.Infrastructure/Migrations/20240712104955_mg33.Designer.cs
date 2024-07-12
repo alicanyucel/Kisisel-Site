c2b374834c7da702b79c855332c5e374989dbb13
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-07-12 13:50:59 +0300

    proje ayağa kaldırıldı.

--- Kisisel-Site/Kisisel-Site.Infrastructure/Migrations/20240712104955_mg33.Designer.cs
+++ Kisisel-Site/Kisisel-Site.Infrastructure/Migrations/20240712104955_mg33.Designer.cs
@@ -4,6 +4,7 @@ using Kisisel_Site.Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Kisisel_Site.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240712104955_mg33")]
+    partial class mg33
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
